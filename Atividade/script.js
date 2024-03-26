eeecb94176404deca7e73b82de759d117f456590
Gustaxbjj <153648794+Gustaxbjj@users.noreply.github.com>
2024-03-26 07:55:36 -0300

    AtividadePOO

---
+++ Atividade/script.js
@@ -0,0 +1,70 @@
+artista1 = {
+   titulo: "Jiu-Jitsu Evolução", 
+   artista: "Billi MC",
+   album: "Single",
+    poster: ""
+}
+
+artista2 = {
+    titulo: "Até Que Durou", 
+    artista: "Pericles",
+    album: "Em sua Direção",
+     poster: ""
+ }
+
+ artista3 = {
+    titulo: "A morte do autotune", 
+    artista: "Matuê",
+    album: "",
+     poster: ""
+ }
+
+ artista4 = {
+    titulo: "", 
+    artista: "",
+    album: "",
+     poster: ""
+ }
+
+ artista5 = {
+    titulo: "", 
+    artista: "",
+    album: "",
+     poster: ""
+ }
+
+ artista6 = {
+    titulo: "", 
+    artista: "",
+    album: "",
+     poster: ""
+ }
+
+
+ artista7 = {
+    titulo: "", 
+    artista: "",
+    album: "",
+     poster: ""
+ }
+
+ artista8 = {
+    titulo: "", 
+    artista: "",
+    album: "",
+     poster: ""
+ }
+
+ artista9 = {
+    titulo: "", 
+    artista: "",
+    album: "",
+     poster: ""
+ }
+
+ artista10 = {
+    titulo: "", 
+    artista: "",
+    album: "",
+     poster: ""
+ }
