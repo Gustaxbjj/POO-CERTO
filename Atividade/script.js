3d725d3d9ff94c8ada5945942a35a7842e0e2a8f
Gustaxbjj <153648794+Gustaxbjj@users.noreply.github.com>
2024-03-26 08:37:45 -0300

    poo

--- Atividade/script.js
+++ Atividade/script.js
@@ -2,69 +2,89 @@ artista1 = {
    titulo: "Jiu-Jitsu Evolução", 
    artista: "Billi MC",
    album: "Single",
-    poster: ""
+    poster: "JiuJitsu.jpg"
 }
 
 artista2 = {
     titulo: "Até Que Durou", 
     artista: "Pericles",
     album: "Em sua Direção",
-     poster: ""
+     poster: "AteQueDurou.jpg"
  }
 
  artista3 = {
     titulo: "A morte do autotune", 
     artista: "Matuê",
-    album: "",
-     poster: ""
+    album: "A morte do autotune",
+     poster: "Autotune.jpg"
  }
 
  artista4 = {
-    titulo: "", 
-    artista: "",
-    album: "",
-     poster: ""
+    titulo: "Os Anjos cantam", 
+    artista: "Jorge e Matheus",
+    album: "Single",
+     poster: "OsAnjosCantam.jpeg"
  }
 
  artista5 = {
-    titulo: "", 
-    artista: "",
-    album: "",
-     poster: ""
+    titulo: "Vibe Meliodas", 
+    artista: " MHRAP",
+    album: "Single",
+     poster: "VibeMelidas.jpg"
  }
 
  artista6 = {
-    titulo: "", 
-    artista: "",
-    album: "",
-     poster: ""
+    titulo: "Maldita de ex", 
+    artista: "MC Leozin",
+    album: "Single",
+     poster: "EX.jpg"
  }
 
 
  artista7 = {
-    titulo: "", 
-    artista: "",
-    album: "",
-     poster: ""
+    titulo: "Lapada dela", 
+    artista: "Grupo Menos é Mais",
+    album: "Churrasquiho Menos é Mais",
+     poster: "LapadaDela.jpg"
  }
 
  artista8 = {
-    titulo: "", 
-    artista: "",
-    album: "",
-     poster: ""
+    titulo: "Vai me dando Corda", 
+    artista: "Grupo Menos é Mais",
+    album: "Churrasquinho Menos é Mais",
+     poster: "Vaimedando"
  }
 
  artista9 = {
-    titulo: "", 
-    artista: "",
-    album: "",
-     poster: ""
+    titulo: "Foi bom, Mas foi ontem", 
+    artista: "Grupo Menos é Mais",
+    album: "Confia",
+     poster: "FoiBom"
  }
 
  artista10 = {
-    titulo: "", 
-    artista: "",
-    album: "",
-     poster: ""
+    titulo: "Bebe e vem me procurar ", 
+    artista: "turma do quintal",
+    album: "SingleAté Que Durou",
+     poster: "VemMEProcurar"
  }
+
+
+let musicas = []
+
+musicas.push(artista1)
+musicas.push(artista2)
+musicas.push(artista3)
+musicas.push(artista4)
+musicas.push(artista5)
+musicas.push(artista6)
+musicas.push(artista7)
+musicas.push(artista8)
+musicas.push(artista9)
+musicas.push(artista10)
+
+
+for (let i = 0; i < musicas.length; index++) {
+    const element =  (`${artista1.titulo}, ${artista2.}`);
+    
+}
