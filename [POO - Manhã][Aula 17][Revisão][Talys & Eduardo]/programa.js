1472c4dfc0158da41737d983d76db3e5bd8948d0
Gustaxbjj <gtalys0007@gmail.com>
2024-07-03 07:12:07 -0300

    poo

---
+++ [POO - Manhã][Aula 17][Revisão][Talys & Eduardo]/programa.js
@@ -0,0 +1,52 @@
+let NotebookA = {
+    marca: 'Acer',
+    modelo: 'AN515-58-58W3',
+    ram: '8gb',
+    processador: 'CORE I5 12450H',
+    armazenamento:'512gb',
+    tamanhoDaTela:'15.6',
+    valor: 'R$4.420',
+}
+let NotebookB = {
+    marca: 'Acer',
+    modelo: 'PH315-54',
+    ram: '16gb',
+    processador: 'CORE I7 11800H',
+    armazenamento:'512gb',
+    tamanhoDaTela:'15.6',
+    valor: 'R$8.249',
+}
+let NotebookC = {
+    marca: 'Dell',
+    modelo: 'Inspiron G15',
+    ram: '16GB',
+    processador: 'AMD Ryzen 7 6800H',
+    armazenamento:'512GB',
+    tamanhoDaTela:'15.6',
+    valor: 'R$7.817',
+}
+let NotebookD = {
+    marca: 'Positivo',
+    modelo: '3053562',
+    ram: '4gb',
+    processador: 'PENTIUM N5030',
+    armazenamento:'128gb',
+    tamanhoDaTela:'14',
+    valor: 'R$1.359',
+}
+let NotebookE = {
+    marca: 'Apple',
+    modelo: 'M1 2020',
+    ram: '8GB',
+    processador: 'Apple M1 8-Core GPU',
+    armazenamento:'256GB',
+    tamanhoDaTela:'13.3',
+    valor: 'R$7.819',
+}
+
+    listaDeNotebook = []
+    listaDeNotebook.push(NotebookA)
+    listaDeNotebook.push(NotebookB)
+    listaDeNotebook.push(NotebookC)
+    listaDeNotebook.push(NotebookD)
+    listaDeNotebook.push(NotebookE)
