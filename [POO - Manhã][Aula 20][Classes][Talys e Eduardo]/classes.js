7dfe889538af027b19193eb1d2ca6b858dd50ace
Gustaxbjj <gtalys0007@gmail.com>
2024-07-10 09:09:25 -0300

    10/07/2024

--- [POO - Manhã][Aula 20][Classes][Talys e Eduardo]/classes.js
+++ [POO - Manhã][Aula 20][Classes][Talys e Eduardo]/classes.js
@@ -1,3 +1,25 @@
- class contaDeEnergia = {
+ class ContaDeEnergia  {
     
+
+    constructor(wConsumo,dataDeVencimento,nomeDoDestinatario,taxaDeiluminacao){
+        this.dataDeVencimento = dataDeVencimento;
+        this.nomeDoDestinatario = nomeDoDestinatario;
+        this.taxaDeiluminacao = taxaDeiluminacao;
+        this.wConsumo = wConsumo;
+        this.total = this.taxaDeiluminacao * this.wConsumo
+    }
+
+
+
  }
+
+ class ContaDeAgua {
+
+    constructor(valor,taxaDeEsgoto, dataDePagamento){
+        this.valor = valor 
+        this.dataDePagamento = dataDePagamento
+        this.taxaDeEsgoto = taxaDeEsgoto
+        this.total = this.valor * this.taxaDeEsgoto
+}
+ }
+ 
---
+++ [POO - Manhã][Aula 20][Classes][Talys e Eduardo]/programa.js
@@ -0,0 +1,5 @@
+let minhaContaDeEnergia = new ContaDeEnergia(20, 0.50, 'Talys', '12/07/2024')
+let minhacontaDeAgua = new ContaDeAgua(50 , 1.5, '12-03-2024')
+
+console.log( `Energia: R$ ${minhaContaDeEnergia.total} -> ${minhaContaDeEnergia.nomeDoDestinatario} -> ${minhaContaDeEnergia.dataDeVencimento}`)
+console.log(`agua: R$ ${minhacontaDeAgua.total} -> ${minhacontaDeAgua.dataDePagamento}`)
