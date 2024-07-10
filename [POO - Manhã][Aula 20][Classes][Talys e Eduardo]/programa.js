7694cbf68671409bc78bdc45ae9370d7ae120017
Gustaxbjj <153648794+Gustaxbjj@users.noreply.github.com>
2024-07-10 11:21:50 -0300

    Atualizar o programa.js

--- [POO - Manhã][Aula 20][Classes][Talys e Eduardo]/programa.js
+++ [POO - Manhã][Aula 20][Classes][Talys e Eduardo]/programa.js
@@ -1,4 +1,4 @@
-let minhaContaDeEnergia = new ContaDeEnergia(20, 0.50, 'Talys', '12/07/2024')
+let minhaContaDeEnergia = new ContaDeEnergia(23,0.50, 'Talys', '12/07/2024')
 let minhacontaDeAgua = new ContaDeAgua(50 , 1.5, '12-03-2024')
 
 console.log( `Energia: R$ ${minhaContaDeEnergia.total} -> ${minhaContaDeEnergia.nomeDoDestinatario} -> ${minhaContaDeEnergia.dataDeVencimento}`)
