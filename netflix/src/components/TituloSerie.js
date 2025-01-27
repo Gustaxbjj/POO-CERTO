fcd2283e2f4d0a6987c06b40684ff47d890aeddc
Gabrielluiz998 <153648794+Gustaxbjj@users.noreply.github.com>
2025-01-27 12:35:12 -0300

    ...

--- netflix/src/components/TituloSerie.js
+++ netflix/src/components/TituloSerie.js
@@ -2,7 +2,7 @@ import  './TituloSerie.css'
 import Nomeserie from './Nomeserie'
 import BtnAssistir from './BtnAssistir';
 import BtnInfo from './BtnInfo';
-import Round6Nome from'../imgHome/Round6Nome.png'
+import '../imgHome/Round6Nome.png'
 import { SiNetflix } from "react-icons/si";
 import { TbNumber16Small } from "react-icons/tb";
 import { FaVolumeHigh } from "react-icons/fa6";
